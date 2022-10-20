5f1b7a891e638fc110d28e8f7fb0af2c4eac9721
ngocanhnt2612@gmail.com <ngocanhnt2612@gmail.com>
2022-10-20 23:45:42 +0700

    20/10/2022

--- khoa_hoc_toan_12.php
+++ khoa_hoc_toan_12.php
@@ -7,23 +7,22 @@
    <title>courses</title>
 
    <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
    <!-- swiper css link  -->
    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
+
 
 </head>
 <body>
    
 <!-- header section starts  -->
 <?php @include 'header.php'; ?>
-
-
-
 <!-- header section ends -->
 
 <section class="heading-link">
@@ -45,53 +44,43 @@
 <?php 
       // Kết nối đến CSDL
       require('config.php');
-      /* $sql="SELECT * FROM `chi_tiet_chuong_trinh` WHERE ct_chuong like 'L12%'";
 
-      $chi_tiet=mysqli_query($con,$sql);
-
-      while ($row=mysqli_fetch_array($chi_tiet))
-      {
-         echo $row["chi_tiet_ten_bai"]."<br>";
-      }
-      */
 ;?>
 
-   <div class="accordion accordion-flush" id="accordionFlushExample">
      <?php 
-     $sql="SELECT * FROM `chi_tiet_chuong_trinh` WHERE ct_chuong like 'L12%'";
+     $sql1="SELECT * FROM `tbl_chuong_trinh` WHERE th_id like 'LOP12'";
+     $chuong_trinh=mysqli_query($con,$sql1);
+     while ($chuong=mysqli_fetch_array($chuong_trinh))
+     {  ;?>
+   
+   <p class="pcourses" style="font-weight:bold; text-transform: uppercase;"> <?php echo $chuong["ct_ten_chuong"];?> </p> 
+    <?php $sql="SELECT * FROM `chi_tiet_chuong_trinh` WHERE ct_chuong ='".$chuong["ct_chuong"]."'";
 
       $chi_tiet=mysqli_query($con,$sql);
-     $i=0; 
      while ($row=mysqli_fetch_array($chi_tiet)) 
-      { if ($i==0) {
+         {
          ?>   
-  <div class="accordion-item">
+   <div class="accordion accordion-flush" id="accordionFlushExample">
+          <div class="accordion-item">
 
-    <h2 class="accordion-header" id="flush-headingOne"> 
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-        <?php echo $row["chi_tiet_ten_bai"];?>
-      </button>
-    </h2>
-    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
-    </div>
-  </div>
-<?php } else { ?>
-  <br>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-heading<?php echo $row["chi_tiet_id"];?>">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $row["chi_tiet_id"];?>" aria-expanded="false" aria-controls="flush-collapse<?php echo $row["chi_tiet_id"];?>">
-        <?php echo $row["chi_tiet_ten_bai"];?>
-      </button>
-    </h2>
-    <div id="flush-collapse<?php echo $row["chi_tiet_id"];?>" class="accordion-collapse collapse" aria-labelledby="flush-heading<?php echo $row["chi_tiet_id"];?>" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
-    </div>
-  </div>
-<?php } $i++;} ?>
+            <h2 class="accordion-header" id="flush-heading<?php echo $row["chi_tiet_id"];?>"> 
+               <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?php echo $row["chi_tiet_id"];?>" aria-expanded="false" aria-controls="flush-collapse<?php echo $row["chi_tiet_id"];?>">
+                <p class="pcourses" style="text-transform: none;"> <?php echo $row["chi_tiet_ten_bai"];?> </p></button></h2>
+              <div id="flush-collapse<?php echo $row["chi_tiet_id"];?>" class="accordion-collapse collapse" aria-labelledby="flush-heading<?php echo $row["chi_tiet_id"];?>" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">
+               <ul>
+                 <li style="font-size: 1.5rem;">Video bài giảng</li>
+                 <li style="font-size: 1.5rem;">Đề ôn tập</li>
+                 <li style="font-size: 1.5rem;">Đáp án</li>
+                 <li style="font-size: 1.5rem; list-style-type: none;"><a href="luu_bai_giang.php">Lưu bài giảng </a> </li>
+               </ul>
+              </div>
+             </div>
+           </div>
+       <?php } ;?> <br>
+   <?php } ?> 
   
     </div>
-   
 </section>
 
 
