994941b747c14b185f52c7278db0e89c100eb369
hoangtrang28 <89293697+hoangtrang28@users.noreply.github.com>
2022-10-24 11:32:57 +0700

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,1 +1,55 @@
-footer
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Trang chủ</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3> <i class="fas fa-lightbulb"></i> Easymath</h3>
+         <p>Website học toán online miễn phí dành cho các khối 10, 11, 12 </p>
+         <div class="share">
+            <a href="https://www.facebook.com/profile.php?id=100043800724081" class="fab fa-facebook-f"></a>
+            <a href="https://www.youtube.com/channel/UCSoASXyNCHHrKe1lcegA4Cw" class="fab fa-youtube"></a>
+            <a href="https://www.instagram.com/doanthat91/" class="fab fa-instagram"></a>
+            <a href="https://www.tiktok.com/@thaythattoan?" class="fab fa-tiktok"></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <h3>Thông tin </h3>
+         <a href="gioi_thieu.php" class="link">Giới thiệu </a>
+         <a href="https://drive.google.com/drive/folders/1MnHBvjyblDZgrduPAoQqCdlMGquka3N4" class="link">Tài liệu học tập</a>
+         <a href="khoa_hoc_lop_10.php" class="link">Khóa học lớp 10</a>
+         <a href="khoa_hoc_lop_11.php" class="link">Khóa học lớp 11</a>
+         <a href="khoa_hoc_lop_12.php" class="link">Khóa học lớp 12</a>
+         <a href="trang_gop_y.php" class="link">Góp ý</a>
+      </div>
+
+      <div class="box">
+            <h3>Liên hệ</h3>
+            <p> <i class="fas fa-phone"></i> Hotline: 18002126 </p>
+            <p> <i class="fas fa-envelope"></i> Email: easymath@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Trụ sở: Học viện Ngân hàng, số 12 Chùa Bộc, Đống Đa, Hà nội</p>
+        </div>
+
+   </div>
+
+   <div class="credit">&copy;  <?php echo date('Y'); ?> Bản quyền thuộc về tác giả. Cấm sao chép dưới mọi hình thức</div>
+
+</section>
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>home</title>
+   <title>Trang chủ</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -22,27 +22,29 @@
 
     <nav class="navbar">
         <ul>
-            <li><a href="#">Trang chủ</a></li>
+            <li><a href="trang_chu.php">Trang chủ</a></li>
             <li><a href="#">Khóa học</a>
                 <ul>
-                    <li><a href="course-1.html">Toán 10</a></li>
-                    <li><a href="course-2.html">Toán 11</a></li>
-                    <li><a href="course-3.html">Toán 12</a></li>
+                    <li><a href="khoa_hoc_lop_10">Toán 10</a></li>
+                    <li><a href="khoa_hoc_lop_11">Toán 11</a></li>
+                    <li><a href="khoa_hoc_lop_12">Toán 12</a></li>
                 </ul>
             </li>
-            <li><a href="contact.html">Góp ý</a></li>
+            <li><a href="trang_gop_y.php">Góp ý</a></li>
             <li><a href="#">Tài khoản</a>
                 <ul>
-                    <li><a href="#">Đăng nhập</a></li>
-                    <li><a href="#">Đăng ký</a></li>
+                    <li><a href="dang_nhap.php">Đăng nhập</a></li>
+                    <li><a href="dang_ky.php">Đăng ký</a></li>
                 </ul>
             </li>
             
         </ul>
     </nav>
     <div class="icons">
-     
-      <a href="trang_ca_nhan.php" class="fas fa-user" style="margin: 1.5rem; font-size: 2.5rem;"></a>
+      <div id="account-btn" class="fas fa-user">
+      
+            
+
    </div>
 </header>
 
