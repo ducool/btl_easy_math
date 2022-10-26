7a9f9a07e1f864ed159039a0f7d760a44c34bb04
ducool <115563902+ducool@users.noreply.github.com>
2022-10-26 22:09:40 +0700

    Add files via upload

--- trang_ca_nhan.php
+++ trang_ca_nhan.php
@@ -1,3 +1,16 @@
+<?php
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:dang_nhap.php');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,14 +27,51 @@
 
 </head>
 <body>
-<?php require('config.php'); ?>
+
 <?php include 'user_header.php'; ?>
 
 
 <section class="courses">
   <h1 class="userheading">Khóa học đã đăng ký</h1>
+<div class="box-container">
 
-  <h1 class="userheading">Bài giảng đã lưu</h1>
+      <?php
+         $sql= "SELECT * FROM `tbl_dang_ky_khoa_hoc` where tk_id='$user_id' ORDER BY dk_created DESC";
+         $khoa_hoc=mysqli_query($con,$sql);
+         
+            while($kh=mysqli_fetch_array($khoa_hoc))
+            {
+               $khoa = $kh['th_id'];
+      ?>
+      <div class="box">
+         <div class="tutor">
+            <div>
+               <h3>Thời gian đăng ký: </h3>
+               <span> <?php echo $kh["dk_created"]; ?> </span>
+            </div>
+         </div>
+         <?php if($khoa=="LOP12") { ?>
+            <h3 class="title" style="text-align: center;">Khóa học Toán 12 </h3>
+         <a href="khoa_hoc_toan_12.php" class="btn">Vào học</a>
+        <?php ;} ?>
+          <?php if($khoa=="LOP11") { ?>
+            <h3 class="title" style="text-align: center;">Khóa học Toán 11 </h3>
+         <a href="khoa_hoc_toan_11.php" class="btn">Vào học</a>
+        <?php ;} ?>
+          <?php if($khoa=="LOP10") { ?>
+            <h3 class="title" style="text-align: center;">Khóa học Toán 10 </h3>
+         <a href="khoa_hoc_toan_10.php" class="btn">Vào học</a>
+        <?php ;} ?>
+      </div>
+      <?php
+         }
+      ?>
+
+   </div>
+
+  <br>
+
+  <p style="font-size: 1.7rem; border: .1rem solid; text-align: center; margin-top: 12rem ; text-transform: none;">"Non sông Việt Nam có trở nên vẻ vang hay không, dân tộc Việt Nam có được vẻ vang sánh vai<br>các cường quốc năm châu được hay không, chính là nhờ một phần lớn ở công học tập của các cháu" <br>_Chủ tịch Hồ Chí Minh_</p>
 
 </section>
 <!-- custom js file link  -->
